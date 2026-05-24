2e11440e7592aa8716b46425d98f7f07aed4332e
Curt D Francis <Curtf0505@gmail.com>
2026-05-24 14:56:11 -0400

    Added text chunker and file extraction

--- demo/src/main/java/com/document_QA/demo/controllers/QA_Controller.java
+++ demo/src/main/java/com/document_QA/demo/controllers/QA_Controller.java
@@ -4,11 +4,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.document_QA.demo.service.DocumentService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 @RequestMapping("/api/document")
 public class QA_Controller {
+    @Autowired
+    private DocumentService documentService;
+
     @PostMapping("/upload")
     public MultipartFile uploadPDF(@RequestPart MultipartFile File) {
 
--- demo/src/main/java/com/document_QA/demo/service/DocumentService.java
+++ demo/src/main/java/com/document_QA/demo/service/DocumentService.java
@@ -1,5 +1,55 @@
 package com.document_QA.demo.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import java.util.List;
+
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+@Service
 public class DocumentService {
 
+    private String fileExtract(MultipartFile File) {
+        try {
+            byte[] fileInputStream = File.getBytes();
+
+            try (PDDocument ExtractedFile = Loader.loadPDF(fileInputStream)) {
+                PDFTextStripper strip = new PDFTextStripper();
+                String fileText = strip.getText(ExtractedFile);
+                return fileText;
+            }
+        } catch (IOException e) {
+            System.err.println("Error occured while extracting File");
+            return null;
+        }
+    }
+
+    private List<String> textChunker(String fileText) {
+        int MAX_CHUNK_SIZE = 500;
+        int Chunk_Overlap = 50;
+        ArrayList<String> Chunks = new ArrayList<>();
+
+        for (int start = 0; start < fileText.length(); start += (MAX_CHUNK_SIZE - Chunk_Overlap)) {
+            String substring = fileText.substring(start, Math.min(start + MAX_CHUNK_SIZE, fileText.length()));
+            Chunks.add(substring);
+        }
+        return Chunks;
+
+    }
+
+    private void saveToDB() {
+        // Supabase handling
+    }
+
+    public void processDocument(MultipartFile File) {
+        String extractedFile = fileExtract(File);
+        textChunker(extractedFile);
+        // Other functions will go here
+    }
 }
